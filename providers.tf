04d841900f3da7a3937baaf54aec14a3d9e784ca
sand <62765804+Sandrest@users.noreply.github.com>
2024-10-18 14:41:53 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ provider "azurem" {
   features{}
   subsciption_id = ARM_SUBSCRIPTION_ID
   client_id = ARM_CLIENT_ID
-  cleint_secret = ARM_CLIENT_SECRET
+  client_secret = ARM_CLIENT_SECRET
   tenant_id = ARM_TENANT_ID
 
 
