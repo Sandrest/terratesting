e270489cc3f18fd92abfc208c91649b0ede1e21f
sand <62765804+Sandrest@users.noreply.github.com>
2024-11-05 09:59:37 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,4 +13,8 @@ terraform {
 
 provider "azurerm" {  
   features{}
+subsciption_id = ARM_SUBSCRIPTION_ID
+  client_id = ARM_CLIENT_ID
+  client_secret = ARM_CLIENT_SECRET
+  tenant_id = ARM_TENANT_ID
 }
