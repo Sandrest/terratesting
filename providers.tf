896384b515c6237cdfefdc53949d0d8dc88af5cf
sand <62765804+Sandrest@users.noreply.github.com>
2024-11-05 09:28:31 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
 provider "azurerm" {  
   features{}
   subsciption_id = ARM_SUBSCRIPTION_ID
